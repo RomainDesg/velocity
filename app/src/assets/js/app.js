8660ec7489117b9259144d3039ab84ad17af723f
Romain Desgouilles <romain.desgouilles@gmail.com>
2019-02-19 09:35:39 +0100

    first commit

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,58 +1,63 @@
-$(document).ready(function(){
-    $.ajax({
-        type:"get",
-        url: "https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=16613431a6af103df05b4f8ff767c26f87858ec8",
-        success: function(data){
-            console.log(data);
-        },
-        error: function(data){
-            console.log("error");
-        }
-    })
-})
-$("#form").on("submit", function(event){
-    event.preventDefault();
-    // Récupérer pseudo du formulaire
-    pseudo = $("input[name=pseudo]").val();
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzMzMiLCJhIjoiY2pzYWFpcXNwMDAxbzN5cGZneGxia3U3ZCJ9.sigYT2nlLnC1siycJ3im-Q';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [4.85,45.75],
+    zoom: 10,
+    });
     
-    // Récupérer password du formulaire
-    password = $("input[type=password]").val();
-    
-    // Stocker dans un tableau les deux valeurs
-    tabForm = [pseudo, password];
-    //console.log(tabForm);
-    
-    // Stocker dans un objet les deux valeurs
-    objForm = {
-        "pseudo": pseudo,
-        "password": password
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {
+                    "message": "Foo",
+                    "iconSize": [30, 30],
+                },
+                "geometry": {
+                        "type": "Point",
+                        "coordinates": [4.85,45.75]
+                }
+            },
+        ]
     }
-    // console.log(objForm);
-    // console.log(objForm.pseudo); // Select attribut pseudo
 
-    serializeForm = $(this).serialize();
-    // console.log(serializeForm);
-    
-    $.ajax({
-        type:"get",
-        url: "script.php",
-        data: serializeForm,
-        success: function(data){
-            console.log(data);
-            data = JSON.parse(data);
+    // var url = 'https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=16613431a6af103df05b4f8ff767c26f87858ec8';
+    // var oXhr = new XMLHttpRequest();
+    // oXhr.onload = function () {
+    //   var data = JSON.parse(this.responseText);
+    //   // ici les données sont exploitables
+    //   console.log('retour : ', data);
+    // };
+    // oXhr.onerror = function (data) {
+    //   console.log('Erreur ...');
+    // };
+    // oXhr.open('GET', url, true);
+    // oXhr.send(null);
 
-            if(data.error){
-                console.log("Erreur de connexion");
-            }else{
-                // Afficher la map
-                $("#map").show();
-                // Supprimer le formulaire
-                $("#form").hide();
-            }
-        },
-        error: function(data){
-            console.log("error");
-        }
-    })
 
-})
+    geojson.features.forEach(function(marker) {
+        // create a DOM element for the marker
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = 'url(https://placekitten.com/g/' + marker.properties.iconSize.join('/') + '/)';
+        el.style.width = marker.properties.iconSize[0] + 'px';
+        el.style.height = marker.properties.iconSize[1] + 'px';
+        
+        // el.addEventListener('click', function() {
+        //     window.alert(marker.properties.message);
+        // });
+        
+        // add marker to map
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+    });
+
+    // map.addControl(new mapboxgl.GeolocateControl({
+    //     positionOptions: {
+    //         enableHighAccuracy: true
+    //     },
+    //     trackUserLocation: true
+    // }));
