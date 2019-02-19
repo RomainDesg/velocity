24ca4a512069b919d76098ab0432a7bcd58b0307
Romain Desgouilles <romain.desgouilles@gmail.com>
2019-02-19 13:16:59 +0100

    markers

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -3,23 +3,39 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzMzMiLCJhIjoiY2pzYWFpcXNwMDAxbzN5cGZne
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [4.85,45.75],
-    zoom: 10,
+    zoom: 13,
     });
 
 $.ajax({
     type: "GET",
     dataType: "JSON",
-    url: "https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=16613431a6af103df05b4f8ff767c26f87858ec8",
+    url: "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=16613431a6af103df05b4f8ff767c26f87858ec8",
 
     success: function(data){
         console.log(data);
+        data.forEach(function(marker) {
+            // create a DOM element for the marker
+            var el = document.createElement('div');
+            el.id = 'marker';
+        console.log(el.id);
+            var popup = new mapboxgl.Popup({ offset: 25 })
+            // .setText(`${marker.address} \n  ${marker.status}`);
+        // el.addEventListener('click', function() {
+        // window.alert(marker.properties.message);
+        // });
         
+        // add marker to map
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.position)
+            .setPopup(popup)
+            .addTo(map);
+        });
     },
     error: function(data){
         console.error();
     }
-
 })
+
 
 
 
